2254c9a4001ab7560fab8190600cd9accb1d8a78
SHAM <shamim369cse@gmail.com>
2021-04-04 22:07:09 -0700

    up

--- app/product/views.py
+++ app/product/views.py
@@ -2,9 +2,15 @@ from django.shortcuts import render, redirect
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
+from .models import Category
+
 # Create your views here.
 class CategoryView(APIView):
 
     def get(self, request):
-        content = {'message': 'Hello, Product Category'}
+        category_list = Category.objects.all()
+        print("------------------------------------\n", category_list)
+
+        a = [x.category_title for x in category_list]
+        content = {'message': a}
         return Response(content)
